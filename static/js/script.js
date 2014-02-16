8e33c7d610cfad97d80cde678660ae60ae3d8e52
Clark DuVall <clark.duvall@gmail.com>
2014-02-15 19:20:43 -0800

    Fix ajax

--- static/js/script.js
+++ static/js/script.js
@@ -2,12 +2,15 @@ $(function() {
   $('form').submit(function(e) {
     e.preventDefault();
 
-    $.getJSON('https://coinbase.com/api/v1' + $('input').val(), {
-      access_token: API_TOKEN
-    }, function(data) {
-      var output = $('#output');
-      output.empty();
-      output.text(JSON.stringify(data, undefined, 2));
+    $.ajax({
+      url: 'https://coinbase.com/api/v1' + $('input').val(),
+      data: {access_token: API_TOKEN},
+      dataType: 'jsonp',
+      success: function(data) {
+        var output = $('#output');
+        output.empty();
+        output.text(JSON.stringify(data, undefined, 2));
+      }
     });
   });
 });
