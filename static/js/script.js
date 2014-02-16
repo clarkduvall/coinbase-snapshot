7f6463e0b568f38a1f04eae1f368d1fd588850c2
Clark DuVall <clark.duvall@gmail.com>
2014-02-15 18:49:21 -0800

    Testing api

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,13 @@
 $(function() {
+  $('form').submit(function(e) {
+    e.preventDefault();
+
+    $.getJSON('https://coinbase.com/api/v1' + $('input').val(), {
+      access_token: API_TOKEN
+    }, function(data) {
+      var output = $('#output');
+      output.empty();
+      output.text(JSON.stringify(data, undefined, 2));
+    });
+  });
 });
